016f49800a1c91af6c7abc77ec79aa9203ab7047
Potter <45678072+cryptowizard0@users.noreply.github.com>
2025-12-25 11:32:53 +0800

    Remove unused imports and mount function from main.go
    Cleaned up cmd/hycli/main.go by deleting unused imports and the unused mount function to improve code clarity and maintainability.

--- cmd/hycli/main.go
+++ cmd/hycli/main.go
@@ -4,19 +4,10 @@ import (
 	"log"
 
 	"github.com/hymatrix/hycli/internal/cli"
-	"github.com/hymatrix/hycli/internal/generator"
-	"github.com/hymatrix/hycli/internal/generator/schema"
 )
 
 func main() {
 	if err := cli.Execute(); err != nil {
 		log.Fatalf("error: %v", err)
 	}
-}
-
-func mount(projectDir string, vmmName string) error {
-	return generator.Mount(schema.Options{
-		ProjectDir: projectDir,
-		VmmName:    vmmName,
-	})
 }
